1e0f94d275bd5f03e470ca43ba56f5524b364c18
shabnam-priyanka <66578322+shabnam-priyanka@users.noreply.github.com>
2020-08-20 14:42:31 -0400

    done

---
+++ map-filter/map.js
@@ -0,0 +1,26 @@
+const companies = [
+    {name: "Company One", category: "Finance", start: 1981, end: 2003},
+    {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+    {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+    {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+    {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+    {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+    {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+    {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+    {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+  ];
+  
+  const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+console.log(ages.map(element=>element*element));
+
+let agesFilter = ages.filter(x => x < 18 && x > 10)
+console.log(agesFilter);
+
+let agesFind = ages.find(x => x < 18 && x > 10)
+console.log(agesFind);
+
+let x = companies.slice(0,3).map(element=>element.name)
+console.log(x);
+
+
+//   numbers.map(element=>element*element)
